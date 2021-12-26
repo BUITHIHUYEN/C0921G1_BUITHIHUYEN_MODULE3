c54c3543c1550d4a3873f8b75b97278ec3a01576
BUITHIHUYEN <huyenbui2345@gmail.com>
2021-12-26 15:39:20 +0700

    upbai

--- bai_10_jsp_va_jstl/bai_tap/simple_caculator/src/main/java/CaculatorServlet.java
+++ bai_10_jsp_va_jstl/bai_tap/simple_caculator/src/main/java/CaculatorServlet.java
@@ -6,20 +6,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "CaculatorServlet",urlPatterns = "/calculate")
+@WebServlet(name = "CaculatorServlet", urlPatterns = "/calculate")
 public class CaculatorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         float firstOperand = Integer.parseInt(request.getParameter("first_operand"));
         float secondOperand = Integer.parseInt(request.getParameter("second_operand"));
         char operator = request.getParameter("operator").charAt(0);
         float result = Caculator.calculate(firstOperand, secondOperand, operator);
-request.setAttribute("firstOperand",firstOperand);
-request.setAttribute("secondOperand",secondOperand);
-request.setAttribute("operator",operator);
-request.setAttribute("result",result);
-request.getRequestDispatcher("display.jsp").forward(request,response);
+        try {
+            request.setAttribute("firstOperand", firstOperand);
+            request.setAttribute("secondOperand", secondOperand);
+            request.setAttribute("operator", operator);
+            request.setAttribute("result", result);
+            request.getRequestDispatcher("display.jsp").forward(request, response);
 
+        } catch (Exception ex) {
+            request.setAttribute("result", ("Error: " + ex.getMessage()));
+            request.getRequestDispatcher("display.jsp").forward(request, response);
+        }
     }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
